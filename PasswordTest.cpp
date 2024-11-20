9fe84d94291e03702c08a93f06ceca683b1127b1
Sarah <scblotcky@csuchico.edu>
2024-11-19 21:37:16 -0800

    int to bool

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -10,7 +10,7 @@ class PasswordTest : public ::testing::Test
 protected:
 	PasswordTest() {}		   // constructor runs before each test
 	virtual ~PasswordTest() {} // destructor cleans up after tests
-	virtual void SetUp() {}	   // sets up before each test (after constructor)
+	virtual void SetUp() {}	   // sets up before each test (after constructor) 
 	virtual void TearDown() {} // clean up after each test, (before destructor)
 };
 
